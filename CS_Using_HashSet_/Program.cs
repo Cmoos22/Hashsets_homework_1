f844c029f0f2c6ea2dc547632549ba4b3fb39d01
carlamkm <carlamoosskole@gmail.com>
2023-03-09 11:26:04 +0100

    last section

--- CS_Using_HashSet_/Program.cs
+++ CS_Using_HashSet_/Program.cs
@@ -1,17 +1,31 @@
 ﻿using System;
-// This is needed for using stack and hashSet
 using System.Collections.Generic;
+using System.Diagnostics;
 
-namespace CS_Using_HashSet_
+namespace CS_Using_HashSet
 {
-    // using code frome https://www.dotnetcurry.com/csharp/1362/hashset-csharp-with-examples
     class Program
     {
+
+        static string[] names = new string[]
+        {
+         "Tejas", "Mahesh", "Ramesh", "Ram", "GundaRam", "Sabnis", "Leena",
+         "Neema", "Sita" , "Tejas", "Mahesh", "Ramesh", "Ram",
+         "GundaRam", "Sabnis", "Leena", "Neema", "Sita" ,
+         "Tejas", "Mahesh", "Ramesh", "Ram", "GundaRam",
+         "Sabnis", "Leena", "Neema", "Sita" , "Tejas",
+         "Mahesh", "Ramesh", "Ram", "GundaRam", "Sabnis",
+         "Leena", "Neema", "Sita",
+         "Tejas", "Mahesh", "Ramesh", "Ram", "GundaRam", "Sabnis"
+
+        };
+
         static void Main(string[] args)
         {
             // SECTION 2. " Eliminating Duplicates in C# HashSet "
+
             Console.WriteLine("Using HashSet");
-            //1. Defining String Array (Note that the string "mahesh" is repeated) 
+            //1. Defining String Array (Note that the string "mahesh" is repeted) 
             string[] names = new string[] {
                 "mahesh",
                 "vikram",
@@ -49,23 +63,24 @@ namespace CS_Using_HashSet_
 
             // SECTION 3. "Modify HashSet Using UnionWith() Method"
 
+            //1.
             string[] names1 = new string[] {
-             "mahesh","sabnis","manish","sharma","saket","karnik"
+                "mahesh","sabnis","manish","sharma","saket","karnik"
             };
 
-            string[] Names2 = new string[] {
-             "suprotim","agarwal","vikram","pendse","mahesh","mitkari"
+            string[] names2 = new string[] {
+                "suprotim","agarwal","vikram","pendse","mahesh","mitkari"
             };
             //2.
-
             HashSet<string> hSetN1 = new HashSet<string>(names1);
             Console.WriteLine("Data in First HashSet");
             foreach (var n in hSetN1)
             {
                 Console.WriteLine(n);
             }
+
             Console.WriteLine("_______________________________________________________________");
-            HashSet<string> hSetN2 = new HashSet<string>(Names2);
+            HashSet<string> hSetN2 = new HashSet<string>(names2);
             Console.WriteLine("Data in Second HashSet");
             foreach (var n in hSetN2)
             {
@@ -100,25 +115,27 @@ namespace CS_Using_HashSet_
             // to subtract all the names mentuned in Names2 wich contains all names from Names2
             // deleting the names : suprotim, agarwal, suprotim, vikram,pendse and mitkari. witch are all the names in Names2
             // so ExceptWith deletes all the Names2 names from the "Data After Union". 
+
             hSetN3.ExceptWith(hSetN2);
             foreach (var n in hSetN3)
             {
                 Console.WriteLine(n);
             }
 
-            //SECTION 5.Modify Hashset using SymmetricExceptWith() method
+            //SECTION 5. "Modify Hashset using SymmetricExceptWith() method"
 
             // this deletes the doublecate names but keeps the rest 
             HashSet<string> hSetN4 = new HashSet<string>(names1);
             Console.WriteLine("_________________________________");
-            Console.WriteLine("Elements in HashSet before using SymmetricExceptWith");
+            Console.WriteLine("Elements in both HashSet before using SymmetricExceptWith");
             Console.WriteLine("_________________________________");
             Console.WriteLine("HashSet 1");
             foreach (var n in hSetN4)
             {
                 Console.WriteLine(n);
             }
-            Console.WriteLine("HashSet 2");
+            Console.WriteLine();
+            Console.WriteLine("HashSet 4");
             foreach (var n in hSetN2)
             {
                 Console.WriteLine(n);
@@ -131,6 +148,143 @@ namespace CS_Using_HashSet_
             {
                 Console.WriteLine(n);
             }
+            // I need to call this in main for it to work - like calling something in awake 
+
+            Get_Add_Performance_HashSet_vs_List();
+            Get_Contains_Performance_HashSet_vs_List();
+            Get_Remove_Performance_HashSet_vs_List();
+
         }
+        static void Get_Add_Performance_HashSet_vs_List()
+        {
+
+            Console.WriteLine("____________________________________");
+            Console.WriteLine("List Performance while Adding Item");
+            Console.WriteLine();
+            List<string> lstNames = new List<string>();
+            var s2 = Stopwatch.StartNew();
+            foreach (string s in names)
+            {
+                lstNames.Add(s);
+            }
+            s2.Stop();
+
+            Console.WriteLine(s2.Elapsed.TotalMilliseconds.ToString("0.000 ms")); Console.WriteLine();
+            Console.WriteLine("Ends Here");
+            Console.WriteLine();
+            Console.WriteLine("____________________________________");
+            Console.WriteLine("HashSet Performance while Adding Item");
+            Console.WriteLine();
+
+            HashSet<string> hStringNames = new HashSet<string>(StringComparer.Ordinal);
+            var s1 = Stopwatch.StartNew();
+            foreach (string s in names)
+            {
+                hStringNames.Add(s);
+            }
+            s1.Stop();
+
+            Console.WriteLine(s1.Elapsed.TotalMilliseconds.ToString("0.000 ms")); Console.WriteLine();
+            Console.WriteLine("Ends Here");
+            Console.WriteLine("____________________________________");
+            Console.WriteLine();
+
+            /*List Performance while Adding Item 0.602 msEnds Here
+            ____________________________________
+            HashSet Performance while Adding Item
+            0.155 ms
+            Ends Here
+            ____________________________________*/
+            // so here hash set are faster then lists I dont know why as this should be the rewerse as the hashset have to compaire them all
+
+        }
+        static void Get_Contains_Performance_HashSet_vs_List()
+        {
+
+            Console.WriteLine("____________________________________");
+            Console.WriteLine("List Performance while checking Contains operation");
+            Console.WriteLine();
+            List<string> lstNames = new List<string>();
+            var s2 = Stopwatch.StartNew();
+            foreach (string s in names)
+            {
+                lstNames.Contains(s);
+            }
+            s2.Stop();
+
+            Console.WriteLine(s2.Elapsed.TotalMilliseconds.ToString("0.000 ms")); Console.WriteLine();
+            Console.WriteLine("Ends Here");
+            Console.WriteLine();
+            Console.WriteLine("____________________________________");
+            Console.WriteLine("HashSet Performance while checking Contains operation");
+            Console.WriteLine();
+
+            HashSet<string> hStringNames = new HashSet<string>(StringComparer.Ordinal);
+            var s1 = Stopwatch.StartNew();
+            foreach (string s in names)
+            {
+                hStringNames.Contains(s);
+            }
+            s1.Stop();
+
+            Console.WriteLine(s1.Elapsed.TotalMilliseconds.ToString("0.000 ms"));
+            Console.WriteLine();
+            Console.WriteLine("Ends Here");
+            Console.WriteLine("____________________________________");
+            Console.WriteLine();
+
+        } /*List Performance while checking Contains operation
+          0.020 ms
+          Ends Here
+        ____________________________________
+        HashSet Performance while checking Contains operation
+        0.037 ms
+        Ends Here
+        ____________________________________   */
+        // here list out preforms hashsets IDK why its turned arround ? I cant figure it out.
+
+        static void Get_Remove_Performance_HashSet_vs_List()
+        {
+
+            Console.WriteLine("____________________________________");
+            Console.WriteLine("List Performance while performing Remove item operation");
+            Console.WriteLine();
+            List<string> lstNames = new List<string>();
+            var s2 = Stopwatch.StartNew();
+            foreach (string s in names)
+            {
+                lstNames.Remove(s);
+            }
+            s2.Stop();
+
+            Console.WriteLine(s2.Elapsed.TotalMilliseconds.ToString("0.000 ms")); Console.WriteLine();
+            Console.WriteLine("Ends Here");
+            Console.WriteLine();
+            Console.WriteLine("____________________________________");
+            Console.WriteLine("HashSet Performance while performing Remove item operation");
+            Console.WriteLine();
+
+            HashSet<string> hStringNames = new HashSet<string>(StringComparer.Ordinal);
+            var s1 = Stopwatch.StartNew();
+            foreach (string s in names)
+            {
+                hStringNames.Remove(s);
+            }
+            s1.Stop();
+
+            Console.WriteLine(s1.Elapsed.TotalMilliseconds.ToString("0.000 ms")); Console.WriteLine();
+            Console.WriteLine("Ends Here");
+            Console.WriteLine("____________________________________");
+            Console.WriteLine();
+            /*List Performance while performing Remove item operation
+            0.610 ms
+            Ends Here
+            ____________________________________
+            HashSet Performance while performing Remove item operation
+            0.001 ms
+            Ends Here
+            ____________________________________ */
+        }
+        // now it finaly makes sence again 
     }
 }
